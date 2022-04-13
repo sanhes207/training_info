2840a6d6a286c9f34fa2f8768b455194780f8a00
MamoruCrysta <amoshenin@mail.ru>
2022-04-13 12:22:33 +0300

    Файл циклов

--- js/script.js
+++ js/script.js
@@ -1,3 +1,8 @@
 "use strict";
 
-const num = 50;
+let num = 50;
+
+while (num <= 55) {
+  console.log(num);
+  num++;
+}
--- js/Условия.js
+++ js/Условия.js
@@ -1,5 +1,6 @@
 "use strict";
 
+// опреторы условия if/else
 if (4==4) {
   console.log("Ok");
 } else {
@@ -7,11 +8,32 @@ if (4==4) {
 }
 
 const num = 50;
-
+// Разветвление условия else if
 if (num < 49) {
   console.log("Error");
 } else if (num > 50) {
   console.log("Много");
 } else {
   console.log("Ok");
+}
+
+// Тернарный опператор, проверяющий услове и выводящий действие правды или несовпадения
+// Тернарным, данный оператор является из-за исользования трех операторов
+// Является единтсвенным в JS
+(num == 50) ? console.log("ok") : console.log("Error");
+
+// Оператор Switch. Всегда используется строгое сравнение
+switch (num) {
+  case 49:
+    console.log('Неверно');
+    break;
+  case 100:
+    console.log('Неверно');
+    break;
+  case 50:
+    console.log('Ok');
+    break;
+  default:
+    console.log('Дефолтный вариант');
+    break;
 }
