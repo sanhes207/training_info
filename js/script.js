2e3ad9a637ce88bd40f5c459217db4982d5e1f23
MamoruCrysta <amoshenin@mail.ru>
2022-04-13 13:25:13 +0300

    Добавлен файл с циклами

--- js/script.js
+++ js/script.js
@@ -2,7 +2,26 @@
 
 let num = 50;
 
-while (num <= 55) {
+//циклы с while
+while (num < 55) {
+  console.log(num);
+  num++;
+}
+
+//цикл с do..while, проходит одну обязательную итерацию
+do {
+  console.log(num);
+  num++;
+} while(num < 55);
+
+//цикл с for
+for (let i = 1; i < 8; i++) {
+  if (i == 5) {
+    continue; // пропуск итерации
+  }
+  if (i == 6) {
+    break;  //выход из цикла
+  }
   console.log(num);
   num++;
 }
