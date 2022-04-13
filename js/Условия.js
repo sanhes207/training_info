e7bc9c20c1de516ff1c7dcf3801e1a32ca90e552
MamoruCrysta <amoshenin@mail.ru>
2022-04-13 12:20:57 +0300

    Добавлен файл условий

---
+++ js/Условия.js
@@ -0,0 +1,17 @@
+"use strict";
+
+if (4==4) {
+  console.log("Ok");
+} else {
+  console.log("Error");
+}
+
+const num = 50;
+
+if (num < 49) {
+  console.log("Error");
+} else if (num > 50) {
+  console.log("Много");
+} else {
+  console.log("Ok");
+}
